fd5d83b6f7bc8e792c180a7f97a2179f805c83fc
anil-masetti <masettianilkumar.com>
2022-02-11 14:50:48 +0530

    commit

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Home" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+app.get('/one', function (req, res) {
+    res.send('{ "response": "This page one" }');
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+app.get('/two', function (req, res) {
+    res.send('{ "response": " This is page two" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
